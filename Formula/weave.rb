7a84c26c87ccc59d3e2aa5fc5ed6f2684742071b
traviolus <42636319+traviolus@users.noreply.github.com>
2025-01-22 15:11:21 +0700

    fix(weave): update repo name

--- Formula/weave.rb
+++ Formula/weave.rb
@@ -4,10 +4,10 @@ class Weave < Formula
   version "0.0.4"
   license "BSD-3-Clause"
   if Hardware::CPU.arm?
-    url "https://github.com/initia-labs/weave-binaries/releases/download/v0.0.4/weave_v0.0.4_darwin_arm64.tar.gz"
+    url "https://github.com/initia-labs/weave/releases/download/v0.0.4/weave_v0.0.4_darwin_arm64.tar.gz"
     sha256 "c80e23db9df9947303c31da8f778b47cbfdcabdf7d069094e467c3b9c1f79b63"
   else
-    url "https://github.com/initia-labs/weave-binaries/releases/download/v0.0.4/weave_v0.0.4_darwin_amd64.tar.gz"
+    url "https://github.com/initia-labs/weave/releases/download/v0.0.4/weave_v0.0.4_darwin_amd64.tar.gz"
     sha256 "1ab0112c48faf4a7fc056882b2c95badc1ba88a1ae5876123d5bbfe96dab4ae6"
   end
 
